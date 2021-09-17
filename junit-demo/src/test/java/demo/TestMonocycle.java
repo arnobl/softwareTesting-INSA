b86815ef43c8cae2525409a816e67023e56f5e77
arnobl <1052622+arnobl@users.noreply.github.com>
2021-09-17 08:55:11 +0200

    up

--- junit-demo/src/test/java/demo/TestMonocycle.java
+++ junit-demo/src/test/java/demo/TestMonocycle.java
@@ -8,23 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMonocycle {
 	Wheel wheel;
+	Monocycle c;
 
 	@BeforeEach
 	void setUp() {
+		c = new Monocycle();
 		wheel = Mockito.mock(Wheel.class);
 		Mockito.when(wheel.getWeight()).thenReturn(100);
 	}
 
 	@Test
 	void testGetWeightWheelNull() {
-		final Monocycle c = new Monocycle();
 		assertEquals(300, c.getWeight());
 	}
 
 	@Test
 	void testGetWeightWheelNotNull() {
-		final Monocycle c = new Monocycle();
 		c.setWheel(wheel);
 		assertEquals(400, c.getWeight());
+		Mockito.verify(wheel, Mockito.atLeastOnce()).getWeight();
 	}
 }
